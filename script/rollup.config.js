d69de85d0f875c4f2cb4581b99c811fe087d15c3
Johnny Wu <karasaya@gmail.com>
2017-12-01 10:02:13 +0800

    first commit

---
+++ script/rollup.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const buble = require('rollup-plugin-buble');
+const fsJetpack = require('fs-jetpack');
+const pjson = require('../package.json');
+
+let banner = `
+/*
+ * ${pjson.name} v${pjson.version}
+ * (c) ${new Date().getFullYear()} @cocos
+ * Released under the MIT License.
+ */
+`;
+
+let dest = './dist';
+let file = 'ui-kit';
+let moduleName = 'uikit';
+
+// clear directory
+fsJetpack.dir(dest, { empty: true });
+
+module.exports = {
+  entry: './index.js',
+  targets: [
+    { dest: `${dest}/${file}.dev.js`, format: 'iife' },
+    { dest: `${dest}/${file}.js`, format: 'cjs' },
+  ],
+  moduleName,
+  banner,
+  external: ['memop'],
+  globals: {'memop': 'window.memop'},
+  sourceMap: true,
+  plugins: [
+    buble(),
+  ]
+};
