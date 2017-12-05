2c9f69c36a432d1be0a9e1897c325f763cd333de
Huabing.Xu <dabingnn@gmail.com>
2017-12-05 11:27:26 +0800

    raw button implementations

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import cc from 'engine-3d';
+import cc from 'engine-3d.js';
+import enums from './lib/enums';
 import ButtonComponent from './lib/button-component';
 
 cc.extensions.add('uikit', {
   modules: {
+    enums,
     ButtonComponent,
   },
   init(app) {
