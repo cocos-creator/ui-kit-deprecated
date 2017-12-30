459fb67956953371c0159e80383185f4b7403185
Huabing.Xu <dabingnn@gmail.com>
2017-12-30 21:14:25 +0800

    toggle and toggle group works

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import cc from 'engine-3d';
 import enums from './lib/enums';
 import ButtonComponent from './lib/button-component';
 import ToggleComponent from './lib/toggle-component';
+import ToggleGroupComponent from './lib/toggle-group-component';
 
 cc.registerComponent('Button', ButtonComponent);
 
