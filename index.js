8b9bc413b7e2332eae81a91eb4c3bfb75a396f59
Huabing.Xu <dabingnn@gmail.com>
2018-01-02 10:46:49 +0800

    add button toggle and toggle group

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import cc from 'engine-3d';
+import cc from 'engine-3d.js';
 import enums from './lib/enums';
 import ButtonComponent from './lib/button-component';
 import ToggleComponent from './lib/toggle-component';
 import ToggleGroupComponent from './lib/toggle-group-component';
 
+if(!cc) {
+  console.error('cc is not defined');
+}
 cc.registerComponent('Button', ButtonComponent);
 
 export default Object.assign({
