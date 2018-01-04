1c021e7254f210ef3fcb49e88a10ef33c877a91d
JoneLau <350791117@qq.com>
2018-01-04 16:57:11 +0800

    add slider

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import enums from './lib/enums';
 import ButtonComponent from './lib/button-component';
 import ToggleComponent from './lib/toggle-component';
 import ToggleGroupComponent from './lib/toggle-group-component';
+import SliderComponent from './lib/slider-component';
 
 if(!cc) {
   console.error('cc is not defined');
@@ -10,6 +11,7 @@ if(!cc) {
 cc.registerComponent('Button', ButtonComponent);
 cc.registerComponent('Toggle', ToggleComponent);
 cc.registerComponent('ToggleGroup', ToggleGroupComponent);
+cc.registerComponent('Slider', SliderComponent);
 
 export default Object.assign({
 }, enums);
