8ca6b5daef22777f6d575fe1baebf69bab5717ac
JoneLau <350791117@qq.com>
2018-01-15 11:40:27 +0800

    update slider from unity

--- examples/src/slider.js
+++ examples/src/slider.js
@@ -1,34 +1,55 @@
 (() => {
   const { cc, app, uikit } = window;
-  const { color4 } = cc.math;
+  const { quat,color4 } = cc.math;
 
   let screen = app.createEntity('screen');
   screen.addComp('Screen');
 
+  let rotation = quat.create();
+  // quat.fromEuler(rotation, 0, 0, 60);
+
+  let tempEnt = app.createEntity('temp');
+  tempEnt.setParent(screen);
+  let tempWidget = tempEnt.addComp('Widget');
+  tempWidget.width = 960;
+  tempWidget.height = 640;
+
   let sliderBg = app.createEntity('bg');
-  sliderBg.setParent(screen);
+  sliderBg.setParent(tempEnt);
   let bgWidget = sliderBg.addComp('Widget');
   bgWidget.width = 300;
-  bgWidget.height = 40;
+  bgWidget.height = 20;
+  // bgWidget.setPivot(0, 0);
+  sliderBg.setWorldRot(rotation);
   let bgSprite = sliderBg.addComp('Image');
   bgSprite.color = color4.new(1, 1, 1, 1);
 
+  let handleArea = app.createEntity('handleArea');
+  handleArea.setParent(tempEnt);
+  let haWidget = handleArea.addComp('Widget');
+  haWidget.width = 300;
+  haWidget.height = 40;
+  // haWidget.setPivot(0, 0);
+  handleArea.setWorldRot(rotation);
+
   let sliderHandle = app.createEntity('handle');
-  sliderHandle.setParent(screen);
+  sliderHandle.setParent(handleArea);
   let handleWidget = sliderHandle.addComp('Widget');
   handleWidget.width = 20;
-  handleWidget.height = 50;
+  handleWidget.height = 40;
   let handleSprite = sliderHandle.addComp('Image');
   handleSprite.color = color4.new(1, 0, 1, 1);
 
   let sliderEnt = app.createEntity('slider');
   sliderEnt.setParent(screen);
   let sliderWidget = sliderEnt.addComp('Widget');
-  sliderWidget.width = 960;
-  sliderWidget.height = 640;
+  sliderWidget.width = 2000;
+  sliderWidget.height = 2000;
   let sliderComp = sliderEnt.addComp('Slider');
-  // sliderComp.setDirection(uikit.VERTICAL);
 
-  sliderComp.init(sliderHandle, sliderBg);
+  sliderComp._init(sliderHandle, handleArea, sliderBg);
+  // sliderComp.direction = uikit.VERTICAL;
+  // sliderComp.minValue = 100;
+  // sliderComp.maxValue = 50;
   window.g_testSlider = sliderEnt;
 })();
