fc5132c76288a041a3d50e0cc4f3c48dd1e1f8be
Johnny Wu <karasaya@gmail.com>
2018-02-02 17:02:43 +0800

    Refine button-component

--- examples/src/button.js
+++ examples/src/button.js
@@ -1,29 +1,50 @@
 (() => {
   const { cc, app } = window;
-  const { color4 } = cc.math;
+  const { vec3, color4 } = cc.math;
+
+  let camEnt = app.createEntity('camera');
+  vec3.set(camEnt.lpos, 10, 10, 10);
+  camEnt.lookAt(vec3.new(0, 0, 0));
+  camEnt.addComp('Camera');
 
   let screen = app.createEntity('screen');
   screen.addComp('Screen');
 
-  let entity = app.createEntity('sprite');
-  entity.setParent(screen);
-  let spriteCmp = entity.addComp('Image');
-  spriteCmp.width = 128;
-  spriteCmp.height = 128;
-  spriteCmp.setAnchors(0.5, 0.5, 0.5, 0.5);
+  // button1
+  {
+    let ent = app.createEntity('button');
+    ent.setParent(screen);
+    let image = ent.addComp('Image');
+    image.width = 200;
+    image.height = 50;
+    image.setOffset(0, 50);
+    image.setAnchors(0.5, 0.5, 0.5, 0.5);
+    let button = ent.addComp('Button');
+    button.target = ent;
+    button.transition = 'color';
+    button.transitionColors.normal = color4.new(1, 0, 1, 1);
+    button.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    button.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    button.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+    button._updateState();
+  }
 
-  let e2 = app.createEntity('button');
-  e2.setParent(screen);
-  let btnWidget = e2.addComp('Widget');
-  btnWidget.width = 128;
-  btnWidget.height = 128;
-  btnWidget.setAnchors(0.5, 0.5, 0.5, 0.5);
-  let btn = e2.addComp('Button');
-  // let btn = e2.addComp('ui.Button');
-  btn.setTargetSprite(spriteCmp);
-  btn.setTransition('button-transition-color-tint');
-  btn.setTransitionColor(color4.new(1, 0, 0, 1), 'button-state-highlight');
-  btn.setTransitionColor(color4.new(1, 1, 0, 1), 'button-state-pressed');
-  btn.setTransitionColor(color4.new(1, 0, 1, 1), 'button-state-disabled');
-  window.g_testBtn = btn;
+  // button2
+  {
+    let ent = app.createEntity('button');
+    ent.setParent(screen);
+    let image = ent.addComp('Image');
+    image.width = 200;
+    image.height = 50;
+    image.setOffset(0, -50);
+    image.setAnchors(0.5, 0.5, 0.5, 0.5);
+    let button = ent.addComp('Button');
+    button.target = ent;
+    button.transition = 'color';
+    button.transitionColors.normal = color4.new(1, 1, 1, 1);
+    button.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    button.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    button.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+    button._updateState();
+  }
 })();
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import cc from 'engine-3d';
+
+import UIElementComponent from './lib/ui-element-component';
 import ButtonComponent from './lib/button-component';
 import ToggleComponent from './lib/toggle-component';
 import ToggleGroupComponent from './lib/toggle-group-component';
@@ -6,10 +8,9 @@ import SliderComponent from './lib/slider-component';
 import ScrollBarComponent from './lib/scrollBar-component';
 import EditBoxComponent from './lib/editBox-component';
 import ScrollViewComponent from './lib/scrollView-component';
+import UISystem from './lib/ui-system';
 
-if(!cc) {
-  console.error('cc is not defined');
-}
+cc.registerComponent('UIElement', UIElementComponent);
 cc.registerComponent('Button', ButtonComponent);
 cc.registerComponent('Toggle', ToggleComponent);
 cc.registerComponent('ToggleGroup', ToggleGroupComponent);
@@ -17,3 +18,5 @@ cc.registerComponent('Slider', SliderComponent);
 cc.registerComponent('ScrollBar', ScrollBarComponent);
 cc.registerComponent('EditBox', EditBoxComponent);
 cc.registerComponent('ScrollView', ScrollViewComponent);
+
+cc.registerSystem('ui', UISystem, 'UIElement', 0);
