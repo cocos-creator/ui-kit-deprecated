385d76c8085292dfe81fd07e192c4cbcb774d155
Johnny Wu <karasaya@gmail.com>
2018-02-07 11:55:25 +0800

    refine toggle component

--- examples/src/toggle.js
+++ examples/src/toggle.js
@@ -1,36 +1,115 @@
 (() => {
-  const app = window.app;
-  const cc = window.cc;
-  const { ui } = cc;
-  const { color4 } = cc.math;
+  const { cc, app } = window;
+  const { vec3, color3, color4, quat } = cc.math;
+
+  let camEnt = app.createEntity('camera');
+  vec3.set(camEnt.lpos, 10, 10, 10);
+  camEnt.lookAt(vec3.new(0, 0, 0));
+  camEnt.addComp('Camera');
+
   let screen = app.createEntity('screen');
   screen.addComp('Screen');
 
-  let entity = app.createEntity('back');
-  entity.setParent(screen);
-  let spriteCmp = entity.addComp('Image');
-  spriteCmp.width = 128;
-  spriteCmp.height = 128;
-  spriteCmp.setAnchors(0.5, 0.5, 0.5, 0.5);
+  // toggle1 (simple)
+  {
+    let ent = app.createEntity('toggle');
+    ent.setParent(screen);
+    let image = ent.addComp('Image');
+    image.width = 40;
+    image.height = 40;
+    image.setOffset(0, 50);
+    image.setAnchors(0.5, 0.5, 0.5, 0.5);
+    let toggle = ent.addComp('Toggle');
+    toggle.transition = 'color';
+    toggle.transitionColors.normal = color4.new(0.8, 0.8, 0.8, 1);
+    toggle.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    toggle.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    toggle.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
 
-  let e2 = app.createEntity('checkMark');
-  e2.setParent(screen);
-  let sprite2 = e2.addComp('Image');
-  sprite2.color = color4.new(1, 0, 0, 1);
-  sprite2.width = 96;
-  sprite2.height = 96;
-  sprite2.setAnchors(0.5, 0.5, 0.5, 0.5);
+    let checker = app.createEntity('checker');
+    checker.setParent(ent);
+    let checkerImage = checker.addComp('Image');
+    checkerImage.color = color4.new(1, 0, 0, 1);
+    checkerImage.setAnchors(0, 0, 1, 1);
+    checkerImage.setMargin(5, 5, 5, 5);
 
-  let e3 = app.createEntity('toggle');
-  e3.setParent(screen);
-  let toggleWidget = e3.addComp('Widget');
-  toggleWidget.width = 128;
-  toggleWidget.height = 128;
-  toggleWidget.setAnchors(0.5, 0.5, 0.5, 0.5);
-  let toggleCmp = e3.addComp('Toggle');
-  toggleCmp.setTargetSprite(spriteCmp);
-  toggleCmp.setCheckMark(sprite2);
+    toggle.background = ent;
+    toggle.checker = checker;
+    toggle._updateState();
+  }
 
-  window.g_testToggle = toggleCmp;
+  // toggle2 (with text)
+  {
+    let entToggle = app.createEntity('toggle-02');
+    entToggle.setParent(screen);
 
+    let widget = entToggle.addComp('Widget');
+    widget.width = 200;
+    widget.height = 40;
+    widget.setOffset(0, -50);
+
+    let toggle = entToggle.addComp('Toggle');
+    toggle.transition = 'color';
+    toggle.transitionColors.normal = color4.new(0.8, 0.8, 0.8, 1);
+    toggle.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    toggle.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    toggle.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+
+    let entBG = app.createEntity('background');
+    entBG.setParent(entToggle);
+    let image = entBG.addComp('Image');
+    image.width = 40;
+    image.height = 40;
+    image.setAnchors(0, 1, 0, 1);
+    image.setPivot(0, 1);
+
+    let entChecker = app.createEntity('checker');
+    entChecker.setParent(entBG);
+    let checkerImage = entChecker.addComp('Image');
+    checkerImage.color = color4.new(1, 0, 0, 1);
+    checkerImage.setAnchors(0, 0, 1, 1);
+    checkerImage.setMargin(5, 5, 5, 5);
+
+    let entLabel = app.createEntity('label');
+    entLabel.setParent(entToggle);
+    let text = entLabel.addComp('Text');
+    text.setAnchors(0, 0, 1, 1);
+    text.setMargin(45, 5, 5, 5);
+    text.text = 'Foobar';
+    text.color = color4.new(0.1, 0.1, 0.1, 1);
+    text.align = 'left-center';
+
+    //
+    toggle.background = entBG;
+    toggle.checker = entChecker;
+    toggle._updateState();
+  }
+
+  // DEBUG
+  app.on('tick', () => {
+    cc.utils.walk(screen, ent => {
+      let color = color3.new(0, 0, 0);
+      let a = vec3.create();
+      let b = vec3.create();
+      let c = vec3.create();
+      let d = vec3.create();
+      let wpos = vec3.create();
+      let wrot = quat.create();
+
+      let widget = ent.getComp('Widget');
+      widget.getWorldCorners(a, b, c, d);
+
+      // rect
+      app.debugger.drawLine2D(a, b, color);
+      app.debugger.drawLine2D(b, c, color);
+      app.debugger.drawLine2D(c, d, color);
+      app.debugger.drawLine2D(d, a, color);
+
+      app.debugger.drawAxes2D(
+        ent.getWorldPos(wpos),
+        ent.getWorldRot(wrot),
+        5.0
+      );
+    });
+  });
 })();
