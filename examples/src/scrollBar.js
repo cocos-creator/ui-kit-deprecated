f79bd2c006cf8b5e196a7dab47f7e3ee86f407ec
JoneLau <350791117@qq.com>
2018-03-06 18:42:14 +0800

    add scrollBar touch event

--- examples/src/scrollBar.js
+++ examples/src/scrollBar.js
@@ -13,44 +13,85 @@
   let rot = quat.create();
   quat.fromEuler(rot, 0, 0, 30);
 
-  let ent = app.createEntity('ent');
-  ent.setParent(screen);
-  // ent.setWorldRot(rot);
-  let bgSprite = ent.addComp('Image');
-  bgSprite.color = color4.new(1, 1, 1, 1);
-  bgSprite.width = 320;
-  bgSprite.height = 60;
-  let scrollBar = ent.addComp('ScrollBar');
+  // horizontal
+  {
+    let ent = app.createEntity('ent');
+    ent.setParent(screen);
+    // ent.setWorldRot(rot);
+    let bgSprite = ent.addComp('Image');
+    bgSprite.color = color4.new(1, 1, 1, 1);
+    bgSprite.setOffset(-150, 0);
+    bgSprite.width = 320;
+    bgSprite.height = 60;
+    let scrollBar = ent.addComp('ScrollBar');
 
-  let area = app.createEntity('area');
-  area.setParent(ent);
-  let areaWidget = area.addComp('Widget');
-  areaWidget.width = 320;
-  areaWidget.height = 60;
-  areaWidget.marginLeft = 10;
-  areaWidget.marginRight = 10;
-  areaWidget.setAnchors(0, 0, 1, 1);
+    let area = app.createEntity('area');
+    area.setParent(ent);
+    let areaWidget = area.addComp('Widget');
+    areaWidget.width = 320;
+    areaWidget.height = 60;
+    areaWidget.marginLeft = 10;
+    areaWidget.marginRight = 10;
+    areaWidget.setAnchors(0, 0, 1, 1);
 
-  let handle = app.createEntity('handle');
-  handle.setParent(area);
-  let handleSprite = handle.addComp('Image');
-  handleSprite.width = 20;
-  handleSprite.height = 60;
-  handleSprite.marginLeft = -10;
-  handleSprite.marginRight = -10;
-  scrollBar.background = handle;
-  scrollBar.transition = 'color';
-  scrollBar.transitionColors.normal = color4.new(0, 1, 1, 1);
-  scrollBar.transitionColors.highlight = color4.new(1, 1, 0, 1);
-  scrollBar.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
-  scrollBar.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
-  scrollBar._updateState();
+    let handle = app.createEntity('handle');
+    handle.setParent(area);
+    let handleSprite = handle.addComp('Image');
+    handleSprite.width = 20;
+    handleSprite.height = 60;
+    handleSprite.marginLeft = -10;
+    handleSprite.marginRight = -10;
+    scrollBar.background = handle;
+    scrollBar.transition = 'color';
+    scrollBar.transitionColors.normal = color4.new(0, 1, 1, 1);
+    scrollBar.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    scrollBar.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    scrollBar.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+    scrollBar._updateState();
 
-  scrollBar.dragArea = screen;
-  scrollBar.handle = handle;
-  // scrollBar.direction = 'vertical';
-  // scrollBar.reverse = true;
-  // scrollBar.scrollAnchor = 0.3;
-  scrollBar.value = 0.1;
+    scrollBar.handle = handle;
+    scrollBar.size = 0.1;
+  }
 
+  // vertical reverse
+  {
+    let ent = app.createEntity('ent');
+    ent.setParent(screen);
+    // ent.setWorldRot(rot);
+    let bgSprite = ent.addComp('Image');
+    bgSprite.color = color4.new(1, 1, 1, 1);
+    bgSprite.width = 60;
+    bgSprite.height = 320;
+    bgSprite.setOffset(200, 0);
+    let scrollBar = ent.addComp('ScrollBar');
+
+    let area = app.createEntity('area');
+    area.setParent(ent);
+    let areaWidget = area.addComp('Widget');
+    areaWidget.width = 60;
+    areaWidget.height = 320;
+    areaWidget.marginTop = 10;
+    areaWidget.marginBottom = 10;
+    areaWidget.setAnchors(0, 0, 1, 1);
+
+    let handle = app.createEntity('handle');
+    handle.setParent(area);
+    let handleSprite = handle.addComp('Image');
+    handleSprite.width = 60;
+    handleSprite.height = 20;
+    handleSprite.marginTop = -10;
+    handleSprite.marginBottom = -10;
+    scrollBar.background = handle;
+    scrollBar.transition = 'color';
+    scrollBar.transitionColors.normal = color4.new(0, 1, 1, 1);
+    scrollBar.transitionColors.highlight = color4.new(1, 1, 0, 1);
+    scrollBar.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
+    scrollBar.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+    scrollBar._updateState();
+
+    scrollBar.handle = handle;
+    scrollBar.direction = 'vertical';
+    scrollBar.reverse = true;
+    scrollBar.size = 0.1;
+  }
 })();
